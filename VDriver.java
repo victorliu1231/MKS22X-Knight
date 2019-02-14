abd84f505c37c0f5fbabcb2f96801156c94275ca
vliu00 <vliu00@stuy.edu>
2019-02-14 14:45:58 -0500

    updating driver:

--- VDriver.java
+++ VDriver.java
@@ -2,6 +2,7 @@ public class VDriver{
     public static void main(String[]args){
         KnightBoard k1 = new KnightBoard(5,5);
         System.out.println(k1);
+        /*
         System.out.println(k1.solve(0,0));
         System.out.println();
         System.out.println(k1);
@@ -19,6 +20,15 @@ public class VDriver{
         System.out.println(k1.countSolutions(4, 4));
         System.out.println(k1);
         System.out.println();
+        System.out.println(k1.countSolutions(2, 2));
+        System.out.println(k1);
+        System.out.println();
+        */
+
+        KnightBoard k2 = new KnightBoard(3,4);
+        System.out.println(k2.solve(2,3)); System.out.println(k2);
+        k2 = new KnightBoard(3,4);
+        System.out.println(k2.countSolutions(2,3)); System.out.println(k2); System.out.println();
         //test exceptions
     }
 }
